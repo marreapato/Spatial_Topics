ab788390d24ffc4756d918cbd86de9dd85332f05
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2022-09-02 07:55:19 -0300

    Update class_1.r

--- class_1.r
+++ class_1.r
@@ -16,6 +16,16 @@ BOR <- s100$coords[chull(s100$coords),]#bordadura
 names(s100)
 s100.bin <- variog(s100)
 plot(s100.bin)
+
+#envelope plot
 s100.bin <- variog(s100, max.dist=1)
 s100.vario.env <- variog.mc.env(s100, obj=s100.bin)
 plot(s100.bin, env=s100.vario.env)
+
+##
+
+s100.fit.exp <- variofit(s100.bin, ini=c(0.9,.35), cov.model="exp",nug=FALSE, fix.kappa=FALSE)
+summary(s100.fit.exp)
+s100.fit.expplot(s100.bin)
+lines(s100.fit.exp,col="green")
+s100.fit.exp
