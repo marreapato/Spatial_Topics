9b2170ae293ca670676686852e6c55ee3faa0e4e
Mariana Almeida <56414513+almeidamariana@users.noreply.github.com>
2022-10-21 08:42:17 -0300

    Update nossa_base.r

--- nossa_base.r
+++ nossa_base.r
@@ -24,7 +24,7 @@ plot(x=dados$x,y=dados$y,pch="o",col=as.factor(dados$grupo),xlab="lat",ylab="lon
 dados$x[1:50]#latitude
 dados$y[1:50]#longitude     
 
-boxplot(dados_nosso$folhas~dados_nosso$flor)
+#boxplot(dados_nosso$folhas~dados_nosso$flor)
 
 dados_nosso <- data.frame(lat=dados$x[1:50],long=dados$y[1:50])
 dados_nosso$folhas <- c(26,4,6,1,6,22,15,6,6,15,12,12,5,6,33,64,2,2,9,6,6,7,8,9,7,17,4,6,8,1,10,6,9,9,4,4,3,2,5,5,4,6,2,NA,12,8,4,5,18,7)
